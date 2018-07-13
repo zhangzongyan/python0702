247611a63d0cd26d2f4eceaefe7781cfb80ebce0
Zhangzongyan <zhangzongyan@uplooking.com>
2018-07-13 11:27:06 +0800

    搜狗安装方式

--- day03/hello.py
+++ day03/hello.py
@@ -1,1 +1,3 @@
+#!/usr/bin/env python
+
 print("hello world")
--- day04/test1.py
+++ day04/test1.py
@@ -1,11 +1,17 @@
 
-# 交互---》输入
-story = input("说出你的故事:")
-
-# 要求:要求故事不能超过6了字
-if len(story) <= 6:
-	# 输出
-	print("人生苦短，我用Python!!", story)
-else:
-	print("墨迹死了！重新说")
+# 循环
+while True:
+	# 交互---》输入
+	story = input("说出你的故事:")
 	
+	# 要求:要求故事不能超过6了字
+	if len(story) <= 6:
+		# 输出
+		print("人生苦短，我用Python!!", story)
+		# 终止循环
+		break
+	else:
+		print("墨迹死了！重新说")
+
+print("很好！你的故事很精彩....")
+		
